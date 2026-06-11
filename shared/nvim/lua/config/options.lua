b49e6a9679392358b4e95dcc96c15da7260e3437
Leandro Cardenas <admin@autanasoft.com>
2026-06-10 22:22:09 -0400

    feat(nvim): unify spell config (es/en) in shared options.lua

--- shared/nvim/lua/config/options.lua
+++ shared/nvim/lua/config/options.lua
@@ -2,3 +2,18 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- Spell: English and Spanish dictionaries for prose-heavy work
+vim.opt.spell = true
+vim.opt.spelllang = "es,en"
+vim.opt.spellfile = vim.fn.stdpath("data") .. "/site/spell/custom.utf-8.add"
+
+-- Toggle spell priority es <-> en
+vim.keymap.set("n", "<leader>sp", function()
+    local langs = vim.opt.spelllang:get()
+    if langs[1] == "es" then
+        vim.opt.spelllang = "en,es"
+    else
+        vim.opt.spelllang = "es,en"
+    end
+end, { desc = "Toggle spelllang es/en priority" })
--- wsl2-fedora/config/nvim/lua/config/local.lua
+++ wsl2-fedora/config/nvim/lua/config/local.lua
@@ -8,16 +8,4 @@
 -- When adding new shared-level settings, edit `shared/nvim/lua/config/*.lua`
 -- instead. Use THIS file only for true wsl2-only deltas.
 
--- Spell: enable es/en for prose-heavy work in Spanish + English
-vim.opt.spell = true
-vim.opt.spelllang = { "es", "en" }
-vim.opt.spellfile = vim.fn.stdpath("data") .. "/site/spell/custom.utf-8.add"
-
--- Toggle spell priority es <-> en
-vim.keymap.set("n", "<leader>ue", function()
-  if vim.opt.spelllang:get()[1] == "es" then
-    vim.opt.spelllang = { "en", "es" }
-  else
-    vim.opt.spelllang = { "es", "en" }
-  end
-end, { desc = "Toggle spell priority es/en" })
+-- Spell config moved to shared/nvim/lua/config/options.lua
