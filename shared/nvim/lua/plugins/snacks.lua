8184b6ec101b71bdb49ed126183a2016ffba5349
AutanaSoft <admin@autanasoft.com>
2026-06-14 23:14:35 -0400

    refactor(nvim): align plugin spec filenames

--- shared/nvim/lua/plugins/snacks.lua
+++ shared/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,13 @@
 return {
 	"folke/snacks.nvim",
 	opts = {
+		picker = {
+			sources = {
+				explorer = {
+					ignored = false,
+				},
+			},
+		},
 		scroll = {
 			enabled = false, -- Disable scrolling animations
 		},
